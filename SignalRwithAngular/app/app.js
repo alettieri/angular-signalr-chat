b9d75cd2800b3267cfc095e73b9aecd31a8edda5
Antonio Lettieri <alettieri@gmail.com>
2014-10-08 16:49:04 -0700

    Tried to address jHint warnings

--- SignalRwithAngular/app/app.js
+++ SignalRwithAngular/app/app.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	// Register the 'app' module.
-	// It's bootstrapped by the ng-app directive
+	// It's bootstrapped by the ng-app directive.
 	angular.module('app', [
 		'services.hub' // Dependency on the SignalR Hub Service module
 	]);
